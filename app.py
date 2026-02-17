4a2c0437210e702cf55446ed2ae19c1ef6e3109e
Shivam <shivam.verma@anviam.com>
2026-02-17 12:51:32 +0530

    Break CI intentionally

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "CI/CD is working 🚀"}
+    # return {"message": "CI/CD is working 🚀"}
+    return {"message": "Broken CI"}
 
 @app.get("/health")
 def health_check():
